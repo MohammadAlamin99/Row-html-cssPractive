4475d2f20f671cca32a61d0e806eb1e7a1a6e86f
Mohammad Al Amin <alamin.cse.99@gmail.com>
2024-09-08 21:42:55 +0600

    add fetures section

--- js/main.js
+++ js/main.js
@@ -15,3 +15,24 @@ function navToggle(){
     }
 }
 
+
+
+// tabs 
+const tabs = document.querySelectorAll('.tab1');
+const contents = document.querySelectorAll('.content');
+
+contents[0].classList.add('active');
+tabs[0].classList.add('active');
+
+tabs.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+        // Hide all contents
+        contents.forEach(content => content.classList.remove('active'));
+        tabs.forEach(t=> t.classList.remove('active'));
+
+        // Show the content that corresponds to the clicked tab
+        tab.classList.add('active');
+        contents[index].classList.add('active');
+    });
+});
+
